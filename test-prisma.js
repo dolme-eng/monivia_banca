729f811832e801b5bfea5593fbb3b65239907446
sveltbold-sudo <sveltbold@gmail.com>
2026-06-26 18:21:59 +0200

    chore: gitignore db files and test scripts

--- test-prisma.js
+++
@@ -1,3 +0,0 @@
-const { PrismaClient } = require('.prisma/client');
-const p = new PrismaClient({});
-p.user.findMany().then(r => console.log('OK:', r.length)).catch(e => console.error('ERR:', e.message)).finally(() => p.$disconnect());
